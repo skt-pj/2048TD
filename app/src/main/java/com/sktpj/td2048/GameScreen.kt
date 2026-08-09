2bcccccca7c5871f8da905aa9ea1201f42a0ffea
skt-pj <134942813+skt-pj@users.noreply.github.com>
2026-08-09 14:30:00 +0900

    Fix Compose weight import

--- app/src/main/java/com/sktpj/td2048/GameScreen.kt
+++ app/src/main/java/com/sktpj/td2048/GameScreen.kt
@@ -14,7 +14,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card

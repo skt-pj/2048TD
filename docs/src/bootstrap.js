35ce0efe028ef9e67fd3edd1f3776e61733228e1
skt-pj <134942813+skt-pj@users.noreply.github.com>
2026-09-12 16:41:41 +0900

    Load ranking retry transport before web game

--- docs/src/bootstrap.js
+++ docs/src/bootstrap.js
@@ -1,3 +1,4 @@
 import "./audio.js";
 import "./bgm.js?v=audio-controls-2";
-import "./main.js?v=audio-controls-2";
+import "./ranking_transport.js?v=ranking-save-1";
+import "./main.js?v=ranking-save-1";

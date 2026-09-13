03d1f810cad009f1b9ba8425ecd10b2fa287393f
skt-pj <134942813+skt-pj@users.noreply.github.com>
2026-09-13 15:46:06 +0900

    Use refined board themes in Playables build

--- ci/build-playables.sh
+++ ci/build-playables.sh
@@ -32,8 +32,8 @@ import "./vfx_phase2.js?v=vfx-phase2-1";
 import "./vfx_phase3.js?v=vfx-phase3-1";
 import "./bgm.js?v=youtube-platform-1";
 import "./help.js?v=settings-help-1";
-import "./board_style_setup.js?v=board-style-1";
-import "./main.js?v=board-style-1";
+import "./board_style_setup.js?v=board-style-2";
+import "./main.js?v=board-style-2";
 PLAYABLES_BOOTSTRAP
 
 # The shared game module contains a standalone-browser ranking initializer.

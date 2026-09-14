a491cc9dce0e5fee7a1666100e52c525658524ba
skt-pj <134942813+skt-pj@users.noreply.github.com>
2026-09-14 22:58:27 +0900

    Deduplicate weapon attack module loading

--- docs/src/bootstrap.js
+++ docs/src/bootstrap.js
@@ -1,4 +1,4 @@
-import "./weapon_attack_system.js?v=weapon-attacks-web-1";
+import "./weapon_attack_system.js?v=weapon-attacks-1";
 import "./weapon_attack_aim.js?v=weapon-attacks-web-1";
 import "./vfx_phase0.js?v=vfx-phase0-2";
 import "./audio.js?v=youtube-platform-1";
